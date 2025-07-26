bb6ee58d118aae8af5181f17ec8f3c39d6dc03ef
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-26 18:15:55 -0400

    Add reference to form input and submit

--- src/domElements.js
+++ src/domElements.js
@@ -1,5 +1,7 @@
 export default class DomElements {
   constructor() {
     this.swapTempButton = document.querySelector('#temp-swap-button');
+    this.inputLocation = document.querySelector('#location-input');
+    this.searchButton = document.querySelector('#search-button');
   }
 }
