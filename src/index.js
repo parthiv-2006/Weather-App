0af24be95af9c6fbfbd976d2e393fbc79622cd3d
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-26 13:37:28 -0400

    Add call to api function and process data in farenheit and celsius

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import './style.css';
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=8N42KG4NYQDW4YHNNDAG7F2QG&contentType=json`,
+      { mode: 'cors' }
+    );
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
+function farenheitData(weatherData) {
+  const processed = {
+    location: weatherData.resolvedAddress,
+    description: weatherData.description,
+    currentTemp: String(weatherData.days[0].temp.toFixed(1)) + '°F',
+    feelsLikeTemp: String(weatherData.days[0].feelslike.toFixed(1)) + '°F',
+  };
+  console.log(processed);
+}
+
+function celsiusData(weatherData) {
+  const processed = {
+    location: weatherData.resolvedAddress,
+    description: weatherData.description,
+    currentTemp: ((weatherData.days[0].temp - 32) * (5 / 9)).toFixed(1) + '°C',
+    feelsLikeTemp:
+      ((weatherData.days[0].feelslike - 32) * (5 / 9)).toFixed(1) + '°C',
+  };
+  console.log(processed);
+}
+
+getWeatherData('Toronto').then((data) => {
+  farenheitData(data);
+});
+
+getWeatherData('Toronto').then((data) => {
+  celsiusData(data);
+});
