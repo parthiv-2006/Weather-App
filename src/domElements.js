0337018c61c2f190037187ab8409c82c5e7dec4d
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-26 19:06:40 -0400

    Add reference to form and weather info card

--- src/domElements.js
+++ src/domElements.js
@@ -3,5 +3,7 @@ export default class DomElements {
     this.swapTempButton = document.querySelector('#temp-swap-button');
     this.inputLocation = document.querySelector('#location-input');
     this.searchButton = document.querySelector('#search-button');
+    this.form = document.querySelector('form');
+    this.weatherInfoDisplay = document.querySelector('.weather-info');
   }
 }
